044db0961a3066c58f8cb510ff097a5a7f92628e
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-03-22 18:27:08 -0400

    feat: add leitura de arquivo a partir da linha de comando, loop para extrair dados de cada linha e criação do arquivo de texto para testes

--- main.c
+++ main.c
@@ -6,15 +6,48 @@
 
 int main(int argc, char* argv[]) {
 
+    //verifica se argumentos foram passados
+    if (argc != 2) {
+        printf("Argumento faltante: %s <nome_do_arquivo>\n", argv[0]);
+        return 1;
+    }
+    
+    //ler o arquivo
+    FILE* arquivo_entrada = fopen(argv[1], "r");
+    if (arquivo_entrada == NULL) {
+        printf("Erro ao ler o arquivo.\n");
+        return 1;
+    }
+
     //cria lista
     lista_eventos_t* lista = NULL;
-    
+    //instancias para os dados dos eventos
+    double tempo;
+    int alvo, tipo;
+    int cont=0;
+
+    //percorre as linhas do arquivo
     printf("Inserindo no início da lista...\n");
-    lista_eventos_adicionar_inicio(criar_evento(1.5, 1, 1), &lista);
-    lista_eventos_adicionar_inicio(criar_evento(2.5, 2, 2), &lista);
+    while (!feof(arquivo_entrada)){
+        cont++;
+        //lê linha;
+        if (fscanf(arquivo_entrada, "%lf\t%d\t%d\n", &tempo, &alvo, &tipo) != 3) {
+            printf("Erro na leitura");
+            break;
+        }
+        
+        //cria evento
+        evento_t* evento = criar_evento(tempo, alvo, tipo);
+
+        if (evento != NULL) lista_eventos_adicionar_inicio(evento, &lista);
+    }
+
+    //libera memoria do arquivo
+    fclose(arquivo_entrada);
+
     lista_eventos_listar(lista);
+    printf("Total de eventos: %d\n", cont);
     printf("---------FIM---------\n");
     
     return 0; 
-
 }
